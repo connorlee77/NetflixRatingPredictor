e40922a43e50a3202f88ee5381d5dae70bc48614
roshcagra <roshan.agrawal95@gmail.com>
2015-05-11 17:59:17 -0700

    Redo time

--- netflix_project/svd.cpp
+++ netflix_project/svd.cpp
@@ -20,80 +20,121 @@ int NUMFEATURES;
 float BETA = 0.4;
 
 float **user_feature_table;
+float *user_rating_deviation_table;
+
 float **movie_feature_table;
+float *movie_rating_deviation_table;
 
 int *dataArray;
 
 const float REG_A = 0.0255;
-const float LEARNING_A = 2 * 0.00267;
+const float LEARNING_A = 0.00267;
 
 const float REG_B = 3.95;
-const float LEARNING_B = 2 * 0.00000311;
+const float LEARNING_B = 0.00000311;
 
 const float REG_C = 0.00231;
-const float LEARNING_C = 2 * 0.00257;
+const float LEARNING_C = 0.00257;
 
 const float REG_D = 0.0255;
-const float LEARNING_D = 2 * 0.000488;
+const float LEARNING_D = 0.000488;
 
 const float REG_E = 0.0929;
-const float LEARNING_E = 2 * 0.000115;
+const float LEARNING_E = 0.000115;
 
-float CURR_TIME_DEV;
-int CURR_BIN_LOC;
+const float REG_F = 0.0476;
+const float LEARNING_F = 0.00564;
+
+const float REG_G = 0.0190;
+const float LEARNING_G = 0.00103;
+
+const float REG_H = 0.000000011;
+const float LEARNING_H = 0.00236;
+
 float CURR_USER_BASE;
 float CURR_USER_ALPHA;
+float CURR_TIME_DEV;
 float CURR_USER_DATE_BIAS;
+float CURR_USER_TIME_SCALE;
+float CURR_USER_DAY_TIME_SCALE;
+
 float CURR_MOVIE_BASE;
+int CURR_BIN_LOC;
 float CURR_MOVIE_BIN_BIAS;
 
+bool printVectorInitInfo = 0;
+bool printPredictionInfo = 0;
+
 void initializeFeatureVectors() {
-    srand (static_cast <unsigned> (time(0)));
-    float randInit;
-    user_feature_table = new float *[TOTAL_USERS];
-    movie_feature_table = new float *[TOTAL_MOVIES];
+    
+    /*
+     * Initialize user_rating_deviation_table
+     */
+    user_rating_deviation_table = new float[TOTAL_USERS];
     
     ifstream inUserBinFile;
-    ifstream inUserTimesBinFile;
     inUserBinFile.open(userOffsetFile, ios::in|ios::binary);
-    inUserTimesBinFile.open(outUserTimeAverages, ios::in|ios::binary);
     
     if(!inUserBinFile.is_open()) {
         fprintf(stderr, "user binary file was not opened!\n");
     }
+    
+    inUserBinFile.seekg (0, ios::beg);
+    
+    inUserBinFile.read(reinterpret_cast<char*> (&user_rating_deviation_table), sizeof(float) * TOTAL_USERS);
+    
+    inUserBinFile.close();
+    
+    /*
+     * End initialization of user_rating_deviation_table
+     */
+    
+    srand (static_cast <unsigned> (time(0)));
+    float randInit;
+    user_feature_table = new float *[TOTAL_USERS];
+    movie_feature_table = new float *[TOTAL_MOVIES];
+
+    ifstream inUserTimesBinFile;
+    
+    inUserTimesBinFile.open(outUserTimeAverages, ios::in|ios::binary);
+    
     if(!inUserTimesBinFile.is_open()) {
         fprintf(stderr, "user times binary file was not opened!\n");
     }
-    inUserBinFile.seekg (0, ios::beg);
     inUserTimesBinFile.seekg (0, ios::beg);
     
     for(int i = 0; i < TOTAL_USERS; i++) {
         /*
          * indices 0 through NUMFEATURES - 1: vector features
-         * index NUMFEATURES: user rating baseline
+         * index NUMFEATURES:
          * index NUMFEATURES + 1: user time deviation coefficient baseline
          * index NUMFEATURES + 2: user time average
-         * index NUMFEATURES + 3 through NUMFEATURES + 3 + TOTAL_DAYS - 1: user day baselines
+         * index NUMFEATURES + 3 through NUMFEATURES + 2 + TOTAL_DAYS: user day baselines
+         * index NUMFEATURES + 3 + TOTAL_DAYS: user constant time scale baseline
+         * index NUMFEATURES + 4 + TOTAL_DAYS through NUMFEATURES + 3 + TOTAL_DAYS + TOTAL_DAYS: user day-varying time scale baseline
          */
-        user_feature_table[i] = new float[NUMFEATURES + 3 + TOTAL_DAYS];
+        user_feature_table[i] = new float[NUMFEATURES + 4 + 2 * TOTAL_DAYS];
         
         randInit = static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(0.1f)));
         
         std::fill(user_feature_table[i], user_feature_table[i] + NUMFEATURES, randInit);
-        inUserBinFile.read(reinterpret_cast<char*> (user_feature_table[i] + NUMFEATURES), sizeof(float));
-        std::fill(user_feature_table[i] + NUMFEATURES + 1, user_feature_table[i] + NUMFEATURES + TOTAL_DAYS, 0);
+        
         inUserTimesBinFile.read(reinterpret_cast<char*> (user_feature_table[i] + NUMFEATURES + 2), sizeof(float));
+        user_feature_table[i][NUMFEATURES + 3 + TOTAL_DAYS] = 1.0f;
+        
     }
     
-    inUserBinFile.close();
     inUserTimesBinFile.close();
     
-    /*
-    for(int i = 0; i < NUMFEATURES + 3 + TOTAL_DAYS; i++) {
-        printf("%f ", user_feature_table[TOTAL_USERS - 1][i]);
+    if(printVectorInitInfo){
+        printf("Vector features, first: %f, last: %f\n", user_feature_table[TOTAL_USERS - 1][0], user_feature_table[TOTAL_USERS - 1][NUMFEATURES - 1]);
+        printf("User rating deviation: %f\n", user_rating_deviation_table[TOTAL_USERS - 1]);
+        printf("User time deviation coefficient: %f\n", user_feature_table[TOTAL_USERS - 1][NUMFEATURES + 1]);
+        printf("User time average: %f\n", user_feature_table[TOTAL_USERS - 1][NUMFEATURES + 2]);
+        printf("User day baselines, first: %f, last: %f\n", user_feature_table[TOTAL_USERS - 1][NUMFEATURES + 3], user_feature_table[TOTAL_USERS - 1][NUMFEATURES + 2 + TOTAL_DAYS]);
+        printf("User constant time scale: %f\n", user_feature_table[TOTAL_USERS - 1][NUMFEATURES + 3 + TOTAL_DAYS]);
+        printf("User day-varying time scale, first: %f, last: %f\n\n", user_feature_table[TOTAL_USERS - 1][NUMFEATURES + 4 + TOTAL_DAYS], user_feature_table[TOTAL_USERS - 1][NUMFEATURES + 3 + 2 * TOTAL_DAYS]);
     }
-    printf("\n");
-    */
     
     ifstream inMovieBinFile;
     inMovieBinFile.open(movieAveragesFile, ios::in|ios::binary);
@@ -115,17 +156,15 @@ void initializeFeatureVectors() {
         randInit = static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(0.1f)));
         std::fill(movie_feature_table[i], movie_feature_table[i] + NUMFEATURES, randInit);
         inMovieBinFile.read(reinterpret_cast<char*> (movie_feature_table[i] + NUMFEATURES), sizeof(float));
-        std::fill(movie_feature_table[i] + NUMFEATURES + 1, movie_feature_table[i] + NUMFEATURES + 32, 0);
     }
     
     inMovieBinFile.close();
     
-    /*
-    for(int i = 0; i < NUMFEATURES + 31; i++) {
-        printf("%f ", movie_feature_table[TOTAL_MOVIES - 1][i]);
+    if(printVectorInitInfo){
+        printf("Vector features, first: %f, last: %f\n", movie_feature_table[TOTAL_MOVIES - 1][0], movie_feature_table[TOTAL_MOVIES - 1][NUMFEATURES - 1]);
+        printf("Movie rating baseline: %f\n", movie_feature_table[TOTAL_MOVIES - 1][NUMFEATURES]);
+        printf("Movie time bin biases, first: %f, last: %f\n", movie_feature_table[TOTAL_MOVIES - 1][NUMFEATURES + 1], movie_feature_table[TOTAL_MOVIES - 1][NUMFEATURES + 30]);
     }
-    printf("\n");
-    */
 }
 
 
@@ -137,15 +176,17 @@ float predictRating(int user, int movie, int date) {
         sum += (user_feature_table[user - 1][i] * movie_feature_table[movie - 1][i]);
     }
     
-    CURR_USER_BASE = user_feature_table[user - 1][NUMFEATURES];
+    
+    CURR_USER_BASE = user_rating_deviation_table[user - 1];
     CURR_USER_ALPHA = user_feature_table[user - 1][NUMFEATURES + 1];
+    float timeDiff = date - user_feature_table[user - 1][NUMFEATURES + 2];
     CURR_USER_DATE_BIAS = user_feature_table[user - 1][NUMFEATURES + 2 + date];
+    CURR_USER_TIME_SCALE = user_feature_table[user - 1][NUMFEATURES + 3 + TOTAL_DAYS];
+    CURR_USER_DAY_TIME_SCALE = user_feature_table[user - 1][NUMFEATURES + 3 + TOTAL_DAYS + date];
     
     CURR_MOVIE_BASE = movie_feature_table[movie - 1][NUMFEATURES];
-    CURR_BIN_LOC = (date/TOTAL_DAYS) * 30 + 1;
+    CURR_BIN_LOC = ((float) date/ (float) TOTAL_DAYS) * 30 + 1;
     CURR_MOVIE_BIN_BIAS = movie_feature_table[movie - 1][NUMFEATURES + CURR_BIN_LOC];
-    
-    float timeDiff = date - user_feature_table[user - 1][NUMFEATURES + 2];
     
     if(timeDiff < 0)
         sign = -1;
@@ -154,13 +195,29 @@ float predictRating(int user, int movie, int date) {
     
     CURR_TIME_DEV = (float) sign * powf(abs(timeDiff), BETA);
     
-    sum += CURR_USER_BASE + CURR_USER_ALPHA * CURR_TIME_DEV + CURR_USER_DATE_BIAS + CURR_MOVIE_BASE + CURR_MOVIE_BIN_BIAS + GLOBAL_AVG_SET1;
+    sum += GLOBAL_AVG_SET1 + CURR_USER_BASE + CURR_USER_ALPHA * CURR_TIME_DEV + CURR_USER_DATE_BIAS + (CURR_MOVIE_BASE + CURR_MOVIE_BIN_BIAS) * (CURR_USER_TIME_SCALE + CURR_USER_DAY_TIME_SCALE);
     
+    if(printPredictionInfo){
+        printf("User, movie, date: %d %d %d\n", user, movie, date);
+        printf("User rating basis: %f\n", CURR_USER_BASE);
+        printf("User time deviation coefficient: %f\n", CURR_USER_ALPHA);
+        printf("User time difference: %f\n", timeDiff);
+        printf("User time deviation basis: %f\n", CURR_TIME_DEV);
+        printf("User date bias: %f\n", CURR_USER_DATE_BIAS);
+        printf("User constant time scale: %f\n", CURR_USER_TIME_SCALE);
+        printf("User varying time scale: %f\n\n", CURR_USER_DAY_TIME_SCALE);
+    
+        printf("Movie rating basis: %f\n", CURR_MOVIE_BASE);
+        printf("Movie bin location: %d\n", CURR_BIN_LOC);
+        printf("Movie bin bias: %f\n\n", CURR_MOVIE_BIN_BIAS);
+    
+        printf("Rating prediction: %f\n\n", sum);
+    }
     return sum;
 }
 
 void computeSVD(float learning_rate, int num_features, int* train_data, int epochs) {
-    LRATE = 2 * learning_rate;
+    LRATE = learning_rate;
     NUMFEATURES = num_features;
     dataArray = train_data;
     
@@ -192,19 +249,25 @@ void computeSVD(float learning_rate, int num_features, int* train_data, int epoc
             }
             
             //Train user rating baseline (user index: NUMFEATURES)
-            user_feature_table[user - 1][NUMFEATURES] += LEARNING_A * (error - REG_A * CURR_USER_BASE);
+            user_rating_deviation_table[user - 1] += LEARNING_A * (error - REG_A * CURR_USER_BASE);
             
             //Train user time deviation baseline (user index: NUMFEATURES + 1)
             user_feature_table[user - 1][NUMFEATURES + 1] += LEARNING_B * (CURR_TIME_DEV * error - REG_B * CURR_USER_ALPHA);
             
-            //Train user day baseline (user index: somewhere in NUMFEATURES + 3 through NUMFEATURES + 2 + TOTAL_DAYS)
+            //Train user day baseline (user index: NUMFEATURES + 2 + date)
             user_feature_table[user - 1][NUMFEATURES + 2 + date] += LEARNING_C * (error - REG_C * CURR_USER_DATE_BIAS);
             
+            //Train user constant time scale baseline (user index: NUMFEATURES + 3 + TOTAL_DAYS)
+            user_feature_table[user - 1][NUMFEATURES + 3 + TOTAL_DAYS] = LEARNING_F * ((CURR_MOVIE_BASE + CURR_MOVIE_BIN_BIAS) * error - REG_F * (CURR_USER_TIME_SCALE - 1.0f));
+            
+            //Train user day-varying time scale baseline (user index: NUMFEATURES + 3 + TOTAL_DAYS + date)
+            user_feature_table[user - 1][NUMFEATURES + 3 + TOTAL_DAYS + date] = LEARNING_G * ((CURR_MOVIE_BASE + CURR_MOVIE_BIN_BIAS) * error - REG_G * CURR_USER_DAY_TIME_SCALE);
+            
             //Train movie rating baseline (movie index: NUMFEATURES)
-            movie_feature_table[movie - 1][NUMFEATURES] += LEARNING_D * (error - REG_D * CURR_MOVIE_BASE);
+            movie_feature_table[movie - 1][NUMFEATURES] += LEARNING_D * ((CURR_USER_TIME_SCALE + CURR_USER_DAY_TIME_SCALE) *error - REG_D * CURR_MOVIE_BASE);
             
             //Train movie time bin baseline (movie index: somewhere in NUMFEATURES + 1 through NUMFEATURES + 30)
-            movie_feature_table[movie - 1][NUMFEATURES + CURR_BIN_LOC] += LEARNING_E * (error - REG_E * CURR_MOVIE_BIN_BIAS);
+            movie_feature_table[movie - 1][NUMFEATURES + CURR_BIN_LOC] += LEARNING_E * ((CURR_USER_TIME_SCALE + CURR_USER_DAY_TIME_SCALE) * error - REG_E * CURR_MOVIE_BIN_BIAS);
             
             if((j + 1) % 1000000 == 0) {
                 printf("%d test points trained.\n", j + 1);
